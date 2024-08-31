ea340425553e7842014d7ee3ff20dbfbfb534185
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-08-31 17:50:34 -0400

    improvements (and big fix)!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use rand::random;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 use statemachine_derive::statemachine;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::time::SystemTime;
 
 statemachine!(
     Project {
@@ -17,6 +21,7 @@ statemachine!(
 );
 
 fn main() {
+    let init_time = SystemTime::now();
     let mut doc = String::new();
     println!("Please write the document:");
     std::io::stdin().read_line(&mut doc).unwrap();
@@ -25,7 +30,7 @@ fn main() {
         doc,
     };
     loop {
-        match client_review(project.submit()) {
+        match client_review(project.submit(), init_time) {
             ClientResponse::Approved(approved_project) => {
                 println!(
                     "The client is happy! The final doc was:\n{}",
@@ -48,11 +53,20 @@ enum ClientResponse {
     Declined(Project<Declined>),
 }
 
-fn client_review(project: Project<Submitted>) -> ClientResponse {
-    let client_approval: f32 = random();
-    if client_approval >= 0.5 {
+fn client_review(project: Project<Submitted>, init_time: SystemTime) -> ClientResponse {
+    let seed = hash_seed(&(project.doc.as_str(), init_time));
+    let mut rng = StdRng::seed_from_u64(seed);
+
+    let client_approval: f32 = rng.gen();
+    if client_approval >= 0.75 {
         ClientResponse::Approved(project.approve())
     } else {
         ClientResponse::Declined(project.decline())
     }
+}
+
+fn hash_seed<T: Hash>(t: &T) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    t.hash(&mut hasher);
+    hasher.finish()
 }
